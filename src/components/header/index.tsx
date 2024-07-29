0f0010812a5818d862ed4a3d1c20aa55b6c7e749
enyllaoliveira <enyllaoliveira@gmail.com>
2024-07-29 09:14:39 -0300

    changed protection route and conditional rendering at header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,37 +1,34 @@
-import { Link } from 'react-router-dom'
-import logo from '../../assets/logo.svg'
-import { FiUser, FiLogIn } from 'react-icons/fi'
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import { FiUser, FiLogIn } from "react-icons/fi";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
 export default function Header() {
-    const signed = false
-    const loadingAuth = false
-    return (
-        <div className='w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4'>
-           <header className='flex w-full items-center justify-between max-w-7xl px-4 mx-auto'>
-           <Link to='/'>
-           <img src={logo} alt="Logo do site" />
-           </Link>
+  const { signed, loadingAuth } = useContext(AuthContext);
+  return (
+    <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4">
+      <header className="flex w-full items-center justify-between max-w-7xl px-4 mx-auto">
+        <Link to="/">
+          <img src={logo} alt="Logo do site" />
+        </Link>
 
-{!loadingAuth && signed && (
-           <Link to='/dashboard'>
-            <div className='border-2 rounded-full p-1 border-gray-900'>
-           <FiUser size={22} color='#000'/>
-           </div>
-            </Link>
-)
-}
+        {!loadingAuth && signed && (
+          <Link to="/dashboard">
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiUser size={22} color="#000" />
+            </div>
+          </Link>
+        )}
 
-{!loadingAuth && !signed && (
-           <Link to='/login'>
-                        <div className='border-2 rounded-full p-1 border-gray-900'>
-
-           <FiLogIn size={22} color='#000'/>
-           </div>
-
-            </Link>
-)
-}
-
-           </header>
-        </div>
-    )
+        {!loadingAuth && !signed && (
+          <Link to="/login">
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiLogIn size={22} color="#000" />
+            </div>
+          </Link>
+        )}
+      </header>
+    </div>
+  );
 }
